0e7425ff0e1f0a74b58cd1f85525de84f46a5a89
migueljose28 <miguelneto275@gmail.com>
2025-11-11 14:01:50 -0300

    organizing code

--- Beginner/day01/_01day.py
+++ Beginner/day01/_01day.py
@@ -3,18 +3,16 @@ class Main():
     def __init__(self, nome: str) -> None:
         self.nome = nome
         self.apelido = "MiguelDev"
-   
+
     def showNome(self):
         print("Welcome "+self.nome + "!")
 
-    
     def inverterNome(self):
-        #desempacotamento de tupla
         self.nome, self.apelido = self.apelido, self.nome
         return self.nome, self.apelido
+
 
 miguel = Main("miguel")
 miguel.showNome()
 nome, apelido = miguel.inverterNome()
 print(nome, apelido)
-
